010001927ecea43419298f18811dbfb4810499ec
IK4MS <ikill4myskill@outlook.com>
2019-10-25 09:31:16 +0200

    fzf.sh - (include symlinks to executables in $PATH)

--- fzf.sh
+++ fzf.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-IFS=:; find $PATH -maxdepth 1 -executable -type f -printf '%f\n' | fzf --cycle --tiebreak=begin,length,index --layout=reverse --border --color=light,fg:#151414,hl:#BA4B3D,bg+:#BA4B3D | xargs swaymsg exec
+IFS=:; find -L $PATH -maxdepth 1 -executable -type f -printf '%f\n' | fzf --cycle --tiebreak=begin,length,index --layout=reverse --border --color=light,fg:#151414,hl:#BA4B3D,bg+:#BA4B3D | xargs swaymsg exec
